d9d56558cb9220564dd5bf4b26f7f906a7f6626d
ShanaDart73 <carole.kayungu@gmail.com>
2023-05-28 13:41:52 +0200

    nextjs background modification

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "47120cc1606788a4"; }
+/******/ 		__webpack_require__.h = function() { return "a8f1b52ded8f7e7d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
