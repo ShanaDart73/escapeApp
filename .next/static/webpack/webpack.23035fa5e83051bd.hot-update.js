b9ca582611945103b51aedc09ae2b75a8dd8fc5e
ShanaDart73 <carole.kayungu@gmail.com>
2023-07-01 23:39:03 +0200

    added top homepage picture

--- .next/static/webpack/webpack.23035fa5e83051bd.hot-update.js
+++ .next/static/webpack/webpack.23035fa5e83051bd.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7ccee4d2f55325b8"; }
+/******/ 	__webpack_require__.h = function() { return "bb59530571a9038e"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
