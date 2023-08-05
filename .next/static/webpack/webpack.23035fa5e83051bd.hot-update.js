8475486e0d29417abdffed7001909aedcc88e379
ShanaDart73 <carole.kayungu@gmail.com>
2023-08-06 00:02:51 +0200

    adjusted homepage layout

--- .next/static/webpack/webpack.23035fa5e83051bd.hot-update.js
+++ .next/static/webpack/webpack.23035fa5e83051bd.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "eef532a03cafca87"; }
+/******/ 	__webpack_require__.h = function() { return "23b9177c93d7d840"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
