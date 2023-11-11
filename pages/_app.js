3cbe298f81aed889845834760b314dff3071d4bf
ShanaDart73 <carole.kayungu@gmail.com>
2023-11-11 11:33:52 +0100

    added nextseo tag globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import '../styles/global.css'
 import { Analytics } from '@vercel/analytics/react'
+import { NextSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }) {
     return(
         <>
             <Component {...pageProps} />
             <Analytics />
+            <NextSeo
+                title="escape"
+                description="Enjoy reading side stories of Lan WangJi and Wei WuXian, the popular characters of Modaozushi."
+            />
         </>
     )
 }
