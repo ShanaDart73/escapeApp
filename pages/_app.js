fa3abc5b56d654629743ae27f06bfd329337a97e
ShanaDart73 <carole.kayungu@gmail.com>
2023-11-12 18:47:43 +0100

    added nextjs DefaultSeo

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,27 @@
 import '../styles/global.css'
 import { Analytics } from '@vercel/analytics/react'
-import { NextSeo } from 'next-seo'
+import { DefaultSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }) {
     return(
         <>
-            <Component {...pageProps} />
-            <Analytics />
-            <NextSeo
+            <DefaultSeo
                 title="escape"
                 description="Enjoy reading side stories of Lan WangJi and Wei WuXian, the popular characters of Modaozushi."
+                openGraph={{
+                    type: 'website',
+                    locale: 'en_IE',
+                    url: 'https://www.url.ie/',
+                    siteName: 'escape',
+                }}
+                twitter={{
+                    handle: '@handle',
+                    site: '@site',
+                    cardType: 'summary_large_image',
+                }}
             />
+            <Component {...pageProps} />
+            <Analytics />
         </>
     )
 }
