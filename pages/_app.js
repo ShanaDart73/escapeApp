b4e289248da1912f4a473f47aa14a89ac76bdac6
ShanaDart73 <carole.kayungu@gmail.com>
2025-07-24 11:24:21 +0200

    fixing bugs

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }) {
                 }}
             />
             <Component {...pageProps} />
-            <Analytics />
+            <Analytics mode="production" />
         </>
     )
 }
