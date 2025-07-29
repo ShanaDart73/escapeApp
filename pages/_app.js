0ce0324736000fe1b070290d67426c27f602bd65
ShanaDart73 <carole.kayungu@gmail.com>
2025-07-29 15:12:03 +0200

    found the bug

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/global.css'
-//import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import { DefaultSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }) {
@@ -22,6 +22,7 @@ export default function App({ Component, pageProps }) {
                 }}
             />
             <Component {...pageProps} />
+            <Analytics />
         </>
     )
 }
